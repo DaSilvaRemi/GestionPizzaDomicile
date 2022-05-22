3c31049231f677743ea6b8c42b969047a20c775c
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2022-05-22 22:46:16 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.gestionrapizz.gestionpizzadomicile {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens org.gestionrapizz.gestionpizzadomicile to javafx.fxml;
+    exports org.gestionrapizz.gestionpizzadomicile;
+}
