31d59cf4f6d91bb06a89e828bd6f7d1b1c8267f2
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2022-06-08 23:29:19 +0200

    change triggers

--- src/main/resources/org/gestionrapizz/gestionpizzadomicile/SQL/create_trigger.sql
+++ src/main/resources/org/gestionrapizz/gestionpizzadomicile/SQL/create_trigger.sql
@@ -1,34 +1,38 @@
-CREATE OR REPLACE PROCEDURE VerifSoldeClientOnCommande(IN id_client INT, IN montantCommande DOUBLE, OUT idStatut INT)
+CREATE OR REPLACE PROCEDURE verif_solde_client_on_commande(IN id_client INT, IN montant_commande DOUBLE, INOUT id_statut INT)
 BEGIN
-    DECLARE totMontantCommandesClient DOUBLE;
-    SET totMontantCommandesClient = (
-        SELECT SUM(Commande.montant) AS totMontantCommandesClient
-        FROM Commande
-        INNER JOIN Livreur ON Commande.id_utilisateur = Livreur.id_utilisateur
-        INNER JOIN Vehicule ON Commande.immatriculation = Vehicule.immatriculation
-        INNER JOIN Client ON Commande.id_utilisateur_1 = Client.id_utilisateur
-        INNER JOIN Statut ON Commande.id_statut = Statut.id_statut
-        WHERE Commande.id_utilisateur_1 = id_client AND Statut.nom = 'En attente' OR Statut.nom = 'En cours de préparation' OR Statut.nom = 'Livraison en cours'
-        GROUP BY Commande.id_utilisateur_1
+    DECLARE tot_montant_commandes_by_client DOUBLE;
+    SET tot_montant_commandes_by_client = (
+        SELECT IFNULL((
+                SELECT SUM(Commande.montant)
+                FROM Commande
+                INNER JOIN Livreur ON Commande.id_utilisateur = Livreur.id_utilisateur
+                INNER JOIN Vehicule ON Commande.immatriculation = Vehicule.immatriculation
+                INNER JOIN Client ON Commande.id_utilisateur_1 = Client.id_utilisateur
+                INNER JOIN Statut ON Commande.id_statut = Statut.id_statut
+                WHERE Commande.id_utilisateur_1 = id_client AND Statut.nom = 'En attente' OR Statut.nom = 'En cours de préparation' OR Statut.nom = 'Livraison en cours'
+                GROUP BY Commande.id_utilisateur_1
+            ),
+                0.0
+            ) AS tot_montant_commandes_by_client
     );
 
-    IF (SELECT Client.solde FROM Client WHERE Client.id_utilisateur = id_client) < montantCommande + totMontantCommandesClient THEN
-        SET idStatut = (SELECT Statut.id_statut FROM Statut WHERE Statut.nom = 'Refusé');
+    IF (SELECT Client.solde < montant_commande + tot_montant_commandes_by_client FROM Client WHERE Client.id_utilisateur = id_client) = 1 THEN
+        SET id_statut = (SELECT Statut.id_statut FROM Statut WHERE Statut.nom = 'Refusé');
     END IF;
 END;
 
-CREATE OR REPLACE PROCEDURE VerifFideliteClientOnCommande(IN id_client INT, OUT montantCommande DOUBLE)
+CREATE OR REPLACE PROCEDURE verif_fidelite_client_on_commande(IN id_new_commande INT, IN id_client INT, INOUT montant_commande DOUBLE)
 BEGIN
-    DECLARE nbPizzaCommander INTEGER;
-    SET nbPizzaCommander = (SELECT COUNT(Contenir.id_pizza) AS nbPizzaCommander FROM Contenir
+    DECLARE nb_pizza_commander INTEGER;
+    SET nb_pizza_commander = (SELECT COUNT(Contenir.id_pizza) AS nb_pizza_commander FROM Contenir
         INNER JOIN Commande on Contenir.id_commande = Commande.id_commande
         INNER JOIN Client on Commande.id_utilisateur_1 = Client.id_utilisateur
+        WHERE Commande.id_utilisateur_1 = id_client
         GROUP BY Commande.id_utilisateur_1
     );
 
-    IF nbPizzaCommander / 10 >= 1 THEN
-
-        SET montantCommande = montantCommande + (SELECT SUM(
+    IF nb_pizza_commander / 10 >= 1 THEN
+        SET montant_commande = montant_commande + (SELECT SUM(
             CASE Taille.nom
                 WHEN 'Naine' THEN ROUND(prix * 0.67, 2)
                 WHEN 'Ogresse' THEN ROUND(prix * 1.33, 2)
@@ -39,16 +43,17 @@ BEGIN
         INNER JOIN Taille ON Taille.id_taille = Produit.id_taille
         INNER JOIN Contenir ON Produit.id_taille = Contenir.id_taille AND Produit.id_pizza = Contenir.id_pizza
         INNER JOIN Commande ON Contenir.id_commande = Commande.id_commande
-        WHERE Commande.id_commande = 1
+        INNER JOIN Statut on Commande.id_statut = Statut.id_statut
+        WHERE Commande.id_commande = id_new_commande AND Statut.nom != 'En attente' OR Statut.nom = 'Refusé'
         ORDER BY Pizza.prix DESC
-        LIMIT nbPizzaCommander);
+        LIMIT nb_pizza_commander);
     END IF;
 END;
 
-CREATE OR REPLACE PROCEDURE VerifRetardCommande(IN dateHeureCommande DATETIME, IN dateHeureLivraison DATETIME,
-                                                OUT montant DOUBLE, OUT retard BOOLEAN)
+CREATE OR REPLACE PROCEDURE verif_retard_commande(IN date_heure_commande DATETIME, IN date_heure_livraison DATETIME,
+                                                INOUT montant DOUBLE, INOUT retard BOOLEAN)
 BEGIN
-    IF dateHeureLivraison IS NOT NULL AND TIMEDIFF(TIME(dateHeureLivraison), TIME(dateHeureCommande)) >= '00:30:00' THEN
+    IF (SELECT TIMESTAMPDIFF(MINUTE, date_heure_commande, date_heure_livraison) >= 30 FROM Commande WHERE date_heure_livraison IS NOT NULL LIMIT 1) = 1 THEN
         SET retard = TRUE;
         SET montant = 0.0;
     END IF;
@@ -59,10 +64,10 @@ CREATE OR REPLACE TRIGGER verifdatas_commande_insert_trigger
     ON Commande
     FOR EACH ROW
 BEGIN
-    #CALL VerifSoldeClientOnCommande(NEW.id_utilisateur_1, NEW.montant, NEW.id_statut);
-
+    CALL verif_solde_client_on_commande(NEW.id_utilisateur_1, NEW.montant, NEW.id_statut);
     IF (SELECT Statut.nom FROM Statut WHERE Statut.id_statut = NEW.id_statut) != 'Refusé' THEN
-        CALL VerifFideliteClientOnCommande(NEW.id_utilisateur_1, NEW.montant);
+        CALL verif_fidelite_client_on_commande( NEW.id_commande, NEW.id_utilisateur_1, NEW.montant);
+         CALL verif_retard_commande(NEW.dateHeure_commande, NEW.dateHeure_livraison, NEW.montant, NEW.retard);
     END IF;
 END;
 
@@ -72,10 +77,10 @@ CREATE OR REPLACE TRIGGER verifdatas_commande_update_trigger
     FOR EACH ROW
 BEGIN
     IF (SELECT Statut.nom FROM Statut WHERE Statut.id_statut = NEW.id_statut) != 'Refusé' THEN
-        CALL VerifSoldeClientOnCommande(NEW.id_utilisateur_1, NEW.montant, NEW.id_statut);
+        CALL verif_solde_client_on_commande(NEW.id_utilisateur_1, NEW.montant, NEW.id_statut);
         IF (SELECT Statut.nom FROM Statut WHERE Statut.id_statut = NEW.id_statut) != 'Refusé' THEN
-            CALL VerifFideliteClientOnCommande(NEW.id_utilisateur_1, NEW.montant);
-            CALL VerifRetardCommande(NEW.dateHeure_commande, NEW.dateHeure_livraison, NEW.montant, NEW.retard);
+            CALL verif_fidelite_client_on_commande(NEW.id_commande, NEW.id_utilisateur_1, NEW.montant);
+            CALL verif_retard_commande(NEW.dateHeure_commande, NEW.dateHeure_livraison, NEW.montant, NEW.retard);
         END IF;
     END IF;
 END;
